e66bfd35dc26b714581a8b3ae9ec8ea4cf7bdf1e
Danidu Bogoda <danidubogoda95@gmail.com>
2023-08-13 17:52:01 +0530

    initial commit

---
+++ Algo4.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Algo4 {
+    public static void main(String[] args) {
+        int[] nums = { 10, 20, 30, 40, 50 };
+
+        for (int i = 0; i < nums.length/2; i++) {
+            int a = nums[i];
+            int b = nums[nums.length-1-i];
+
+            nums[nums.length-1-i] = a;
+            nums[i] = b;
+            
+        }
+        
+        System.out.println(Arrays.toString(nums));
+    }
+}
